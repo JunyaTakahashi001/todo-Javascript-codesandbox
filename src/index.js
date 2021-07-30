2f76c6cb9f12e197043e65f62cd95b94fe627860
jyunya-takahashi <67084292+jyunya-takahashi@users.noreply.github.com>
2021-07-30 23:45:35 +0900

    入力値の取得&追加

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  //　テキストBOXの値を取得
+  const inputText = document.getElementById("add-text").value;
+  // 入力値が取得できたので、入力欄を空に初期化
+  document.getElementById("add-text").value = "";
+
+  /**
+   * DOMを作成[createElement]HTMLタグを生成
+   */
+  // liタグ作成
+  const li = document.createElement("li");
+  li.innerText = inputText; // liタグに取得した入力値をセット
+
+  // divタグ作成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  // 作成した要素に子要素に設定
+  div.appendChild(li);
+
+  // 未完了のリスト(ulタグ)に追加
+  document.getElementById("incomplete-list").appendChild(div);
+};
+
+// 追加ボタンのイベントリスナー
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
+/**
+ * [getElementById]HTMLの対象要素を取得
+ * [addEventListener]clickイベントを付与する
+ */
