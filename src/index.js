afa4c34580125c5bd4f3ac26856fc5b25cb58f58
jyunya-takahashi <67084292+jyunya-takahashi@users.noreply.github.com>
2021-07-31 08:46:28 +0900

    タスクの完了、削除ボタン追加

--- src/index.js
+++ src/index.js
@@ -17,8 +17,26 @@ const onClickAdd = () => {
   const div = document.createElement("div");
   div.className = "list-row";
 
+  // button(完了)タグ作成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  // 生成と同時にイベントリスナーを付与
+  completeButton.addEventListener("click", () => {
+    alert("完了");
+  });
+
+  // button(削除)タグ作成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  // 生成と同時にイベントリスナーを付与
+  deleteButton.addEventListener("click", () => {
+    alert("削除");
+  });
+
   // 作成した要素に子要素に設定
   div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
 
   // 未完了のリスト(ulタグ)に追加
   document.getElementById("incomplete-list").appendChild(div);
